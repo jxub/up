40b0cae47949329052ac4bf0674599243c3971ed
TJ Holowaychuk <tj@vision-media.ca>
2017-11-06 15:34:02 -0800

    refactor secrets loading, add duration

--- platform/lambda/runtime/runtime.go
+++ platform/lambda/runtime/runtime.go
@@ -2,10 +2,12 @@ package runtime
 
 import (
 	"os"
+	"time"
 
 	"github.com/apex/log"
 	"github.com/apex/up"
 	"github.com/apex/up/internal/secret"
+	"github.com/apex/up/internal/util"
 	"github.com/pkg/errors"
 )
 
@@ -38,12 +40,26 @@ func WithLogger(l log.Interface) Option {
 
 // Init implementation.
 func (r *Runtime) Init(stage string) error {
-	log.Info("loading secrets")
+	if err := r.loadSecrets(stage); err != nil {
+		return errors.Wrap(err, "loading secrets")
+	}
+
+	return nil
+}
+
+// loadSecrets loads secrets.
+func (r *Runtime) loadSecrets(stage string) error {
+	start := time.Now()
+
+	log.Info("initializing secrets")
+	defer func() {
+		log.WithField("duration", util.MillisecondsSince(start)).Info("initialized secrets")
+	}()
 
 	// TODO: all regions
 	secrets, err := NewSecrets(r.config.Name, stage, r.config.Regions[0]).Load()
 	if err != nil {
-		return errors.Wrap(err, "loading secrets")
+		return err
 	}
 
 	secrets = secret.FilterByApp(secrets, r.config.Name)
